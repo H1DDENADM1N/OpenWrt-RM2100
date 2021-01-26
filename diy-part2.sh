1b04343c48f3811c684d2316e47d3d1f4c7907a9
H1DDENADM1N <65646535+H1DDENADM1N@users.noreply.github.com>
2021-01-27 02:08:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,3 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # Modify default hostname
 sed -i 's/OpenWrt/RM2100/g' package/base-files/files/bin/config_generate
-# Modify default theme
-sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
