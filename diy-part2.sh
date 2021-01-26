cefd674639f439110c116cf84e8fa278af0a1572
H1DDENADM1N <65646535+H1DDENADM1N@users.noreply.github.com>
2021-01-26 21:23:55 +0800

    Modify default hostname & theme

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# Modify default hostname
+sed -i 's/OpenWrt/RM2100/g' package/base-files/files/bin/config_generate
+# Modify default theme
+sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
